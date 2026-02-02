59d6ab0b031f7f58b48223611d0b38e21533f784
CheatsMaster <139261034+CheatsMaster@users.noreply.github.com>
2026-02-02 20:52:34 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,11 +2,7 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
-import os
-from dotenv import load_dotenv
 from config import BOT_TOKEN
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # Настройка логирования
 logging.basicConfig(
@@ -58,4 +54,5 @@ if __name__ == "__main__":
     except Exception as e:
 
         logger.error(f"Ошибка при запуске бота: {e}")
+
 
